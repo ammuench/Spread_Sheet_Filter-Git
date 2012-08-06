a2eece15b6bac2e80fc547476c8c89b6f39b8f6b
Alex Muench <ammuench@gmail.com>
2012-08-05 21:28:46 -0400

    Regex Working
    Program parses input and filters out according to filter terms.  Still
    no save function.  Output commands will need some polish.  Test
    mutli-word filter terms.

--- SpreadSheetFilter/filepaths.cs
+++ SpreadSheetFilter/filepaths.cs
@@ -9,6 +9,7 @@ namespace SpreadSheetFilter
     {
         string sourcePath;
         string outputPath = "output.csv";
+        public Boolean fileSuccess = false;
         public void setSource(string FilePathIn)
         { // 
             this.sourcePath = FilePathIn;
@@ -24,6 +25,22 @@ namespace SpreadSheetFilter
         public string getOutput()
         {
             return this.outputPath;
+        }
+        public void testFile()
+        {  
+            try
+            {
+                string[] test = System.IO.File.ReadAllLines(this.sourcePath);
+                fileSuccess = true;
+            }
+            catch (System.IO.IOException)
+            {
+                System.Windows.Forms.MessageBox.Show("File open in another program.  Please close file and try again");
+            }//end catch opened file
+            catch
+            {
+                System.Windows.Forms.MessageBox.Show("Unknown error reading file, please try again");
+            }//end catchall
         }
     }
 }
--- SpreadSheetFilter/filterMain.cs
+++ SpreadSheetFilter/filterMain.cs
@@ -42,75 +42,66 @@ namespace SpreadSheetFilter
 
         private void filterButton_Click(object sender, EventArgs e)
         {
-            outputText.Text = "";
-            outputText.Text = "Starting New Filter\n";
-            try
+            paths.testFile();
+            if (paths.fileSuccess == true)
             {
-                string[] test = System.IO.File.ReadAllLines(paths.getSource());
-                outputText.AppendText("File Load Successful");
-            }
-            catch (System.IO.IOException)
-            {
-                System.Windows.Forms.MessageBox.Show("File open in another program.  Please close file and try again");
-                outputText.AppendText("File Load Error: File open in other program\n");
-            }//end catch opened file
-            catch
-            {
-                System.Windows.Forms.MessageBox.Show("Unknown error reading file, please try again");
-                outputText.AppendText("File Load Error: Unknown Error");
-            }//end catchall
-            string[] source = System.IO.File.ReadAllLines(paths.getSource());
-            int termIndex = 0;
-            foreach (var item in termList.Items)
-            {
-                termIndex++;
-            }//END FOREACH
-            string[] filterTerms = new string[termIndex];
-            termIndex = 0;
-            foreach (var item in termList.Items)
-            {
-                filterTerms[termIndex] = item.ToString();
-                termIndex++;
-            }//END FOREACH
-            int sIndex = 0;
-            int regIndex = 0;
-            
-            outputText.Text = "";
-            foreach (string s in source)
-            {
-                regIndex = 0;
-                int regSuccess = 0;
-                foreach (string reg in filterTerms)
+                outputText.Text = "";
+                string[] source = System.IO.File.ReadAllLines(paths.getSource());
+                int termIndex = 0;
+                foreach (var item in termList.Items)
                 {
-                    outputText.Text = " regex process started";
-                    Match regex = Regex.Match(source[sIndex], filterTerms[regIndex]);
-                    regIndex++;
-                    if (regex.Success)
-                    {
-                        regSuccess++;
-                    }//regex success condition
-                    else
-                    {
-                        regSuccess = -100;
-                    }
-                    
-                }//end foreach
-                if (regSuccess > 0)
+                    termIndex++;
+                }//END FOREACH
+                string[] filterTerms = new string[termIndex];
+                termIndex = 0;
+                foreach (var item in termList.Items)
                 {
-                    outputText.AppendText(source[sIndex] + "\n");
-                }
-            }//Foreach done
-            //FOLLOWING CODE IS JUST TO TEST CREATION OF TERM ARRAY
-            //COMMENT IN TO TEST
-            //THESE COMMENTS ARE JUST SO FULL OF RAGE RIGHT NOW
-            /*int i = 0;
-            foreach (string s in filterTerms)//temporary just to show proper output
-            {
-                outputText.AppendText(filterTerms[i] + "\n");
-                i++;
-            }//end foreach*/
-        }
+                    filterTerms[termIndex] = item.ToString();
+                    termIndex++;
+                }//END FOREACH
+                int sIndex = 0;
+                int regIndex = 0;
 
+                //outputText.Text = "arrays made\n";
+                foreach (string s in source)
+                {
+                    regIndex = 0;
+                    int regSuccess = 0;
+                    regSuccess = 0;
+                    foreach (string reg in filterTerms)
+                    {
+                        //outputText.Text = " regex process started\n";
+                        Match regex = Regex.Match(source[sIndex], filterTerms[regIndex]);
+                        regIndex++;
+                        if (regex.Success)
+                        {
+                            regSuccess++;
+                        }//regex success condition
+                        else
+                        {
+                            regSuccess = -100;
+                        }
+
+                    }//end foreach
+                    if (regSuccess > 0)
+                    {
+                        outputText.AppendText(source[sIndex] + "\n");
+                    }
+                    sIndex++;
+                }//Foreach done
+                //FOLLOWING CODE IS JUST TO TEST CREATION OF TERM ARRAY
+                //COMMENT IN TO TEST
+                //THESE COMMENTS ARE JUST SO FULL OF RAGE RIGHT NOW
+                /*int i = 0;
+                foreach (string s in filterTerms)//temporary just to show proper output
+                {
+                    outputText.AppendText(filterTerms[i] + "\n");
+                    i++;
+                }//end foreach*/
+
+            }//endif
+
+        }//end buttonclick
         private void outputText_TextChanged(object sender, EventArgs e)
         {
             
